7ee5cae02bc7d4d0458a8facd9f3050e5402486b
Vincent <dark-vince15@hotmail.fr>
2015-05-20 15:56:46 +0000

    Test simple script

--- prompt.sh
+++ prompt.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-# Add git branch if its present to PS1
-parse_git_branch() {
- git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
-if [ "$color_prompt" = yes ]; then
- PS1='
-[\t] ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\] : \[\033[01;34m\]\w\[\033[01;31m\] $(parse_git_branch)\[\033[00m\]
-\$ '
-else
- PS1='
-[\t] ${debian_chroot:+($debian_chroot)}\u@\h :\w$(parse_git_branch)
-\$ '
-fi
-echo $PS1
-unset color_prompt force_color_prompt
+# bash <(curl -Ls http://git.io/vTEo9)
+echo 'bash <(curl -Ls http://git.io/vTEo9)'
+exit
