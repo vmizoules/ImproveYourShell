2ca6aab8cbf78c918ad687a40d1c834cb6b1fe66
vmizoules <vmizoules@users.noreply.github.com>
2015-05-20 23:12:43 +0200

    chg txt

--- prompt.sh
+++ prompt.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+# Command to launche :
 # bash <(curl -Ls http://git.io/vTEo9)
-echo 'Hello, you launched the following command:'
-echo 'bash <(curl -Ls http://git.io/vTEo9)'
+
+echo 'Hello from GitHub, you launched the script :'
+echo 'https://github.com/vmizoules/TestRepo/blob/master/prompt.sh'
+
 exit
