a51a72ec6073e49d691aa0c84bad0ad7fe0ccdb5
vmizoules <vmizoules@users.noreply.github.com>
2015-05-20 23:15:31 +0200

    Update prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Command to launche :
-# bash <(curl -Ls http://git.io/vTEo9)
+#Command to launch
+#bash <(curl -Ls http://git.io/vTEo9)
 echo 'Hello from GitHub, you launched the script :'
 echo 'https://github.com/vmizoules/TestRepo/blob/master/prompt.sh'
-exit
