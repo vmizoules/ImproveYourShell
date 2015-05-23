bda0dcdbb1a558a8756bb1b1e3fe2bc0caa017a6
Vincent <dark-vince15@hotmail.fr>
2015-05-23 10:27:31 +0000

    BashCompletion

---
+++ bashrcCompletion.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USER=`whoami`
+BASHRC=".bashrc"
+BASHCOMP=".bash_completion"
+PATHRC=$HOME"/"$BASHRC
+PATHCOMP=$HOME"/"$BASHCOMP
+ERROR=0
+
+WATERMARK="#bashRcCompletionByVMizoules"
+BASHCOMPSCRIPT='echo -e && echo "Bash modded by vmizoules" && echo -e'
+
+# .bashrc exists
+if test -f $PATHRC ; then
+	# if script already setted up
+	if grep "$WATERMARK" $PATHRC >/dev/null ; then
+		echo "Script already executed"
+		echo "Updating $BASHCOMP"
+		# if .bash_completion exists -> remove it
+		if test -f $PATHCOMP ; then
+			rm $PATHCOMP
+		fi
+		# update .bash_completion
+		echo $BASHCOMPSCRIPT > $PATHCOMP
+	else
+		echo "First execute"
+		# mod .bashrc
+		echo -e >> $PATHRC
+		echo $WATERMARK >> $PATHRC
+		echo "if test -f $PATHCOMP ; then" >> $PATHRC
+		echo "    . $PATHCOMP" >> $PATHRC
+		echo "fi" >> $PATHRC
+		# create .bash_completion
+		echo $BASHCOMPSCRIPT > $PATHCOMP
+	fi
+else
+	echo ".bashrc doesn't exist"
+	ERROR=1
+fi
+
+exit $ERROR
