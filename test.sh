ba0ebb23798892a8509f32a24f52da33a3dbd700
vmizoules <vmizoules@users.noreply.github.com>
2015-08-18 16:22:30 +0200

    Remove useless file

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# launch by taping :
-# bash <(curl -Ls http://git.io/vTwMJ)
-
-echo 'Hello from GitHub'
-echo 'This script is hosted online'
-echo 'https://github.com/vmizoules/ShortCommands/'
